f7d9327447efbea8ff4154f1c36b957fd1c2ce8d
leeg <13294679+leegcc@users.noreply.github.com>
2023-09-09 21:51:21 +0800

    init commit

---
+++ src/test/java/cc/leeg/jenkinstest/JenkinsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.leeg.jenkinstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
